0ca1e0fa5e12d43cc5b53e946d42acf294d8b7eb
Ernest Chrzan <echrzan2@smi.pl>
2024-12-04 18:31:06 +0100

    Formats all code using prettier

--- index.ts
+++ index.ts
@@ -1,29 +1,29 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import connectDatabase from '@configs/db.config';
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import connectDatabase from '@configs/db.config'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const PORT = process.env.PORT || 3000;
+const app: Express = express()
+const PORT = process.env.PORT || 3000
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 
 // Routes
 app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'ok' });
-});
+    res.json({ message: 'ok' })
+})
 
 // Start server
 async function startServer() {
-  try {
-    await connectDatabase();
-    app.listen(PORT, () =>
-      console.log(`Server running on http://localhost:${PORT}`),
-    );
-  } catch (error) {
-    console.error('Failed to start the server:', error);
-  }
+    try {
+        await connectDatabase()
+        app.listen(PORT, () =>
+            console.log(`Server running on http://localhost:${PORT}`)
+        )
+    } catch (error) {
+        console.error('Failed to start the server:', error)
+    }
 }
-startServer();
+startServer()
--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,20 +1,20 @@
-import * as mongoose from 'mongoose';
-import * as process from 'node:process';
+import * as mongoose from 'mongoose'
+import * as process from 'node:process'
 
 async function connectDatabase() {
-  const uri = process.env.MONGO_URI as string;
+    const uri = process.env.MONGO_URI as string
 
-  if (!uri) {
-    throw new Error('MongoDB URI is not defined in the .env file');
-  }
+    if (!uri) {
+        throw new Error('MongoDB URI is not defined in the .env file')
+    }
 
-  try {
-    await mongoose.connect(uri);
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(uri)
+        console.log('MongoDB connected successfully')
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error)
+        process.exit(1)
+    }
 }
 
-export default connectDatabase;
+export default connectDatabase
