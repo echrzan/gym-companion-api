873f02d0e018145af02d06ee0a0cae7c18860cc7
Ernest Chrzan <echrzan2@smi.pl>
2024-12-04 18:39:53 +0100

    Formats code after prettier changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     eslint.configs.recommended,
@@ -8,4 +8,4 @@ export default tseslint.config(
     {
         ignores: ['node_modules', 'dist'],
     }
-)
+);
